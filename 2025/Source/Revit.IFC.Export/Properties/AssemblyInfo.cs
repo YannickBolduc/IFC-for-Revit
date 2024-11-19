67519f92ef169285e1ad65da8ef839286399adf5
AMCBRIDGE\ksloboda <ksloboda@amcbridge.com>
2024-11-19 14:39:15 +0200

    Use revit-ifc release 25.2.0 for Revit 2025

---
+++ 2025/Source/Revit.IFC.Export/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+#region Using directives
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IFC Exporter for Revit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Autodesk")]
+[assembly: AssemblyProduct("IFC Exporter for Revit")]
+[assembly: AssemblyCopyright("© 2012-2024 Autodesk, Inc. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("25.2.0.5")]
+[assembly: AssemblyFileVersion("25.2.0.5")]
+
+
+// Version information can now be found in Source\Foundation\RevitENU\Version.cs
+//
+#endregion
